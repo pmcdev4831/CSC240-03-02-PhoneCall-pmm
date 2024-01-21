383db6af92a3182d6c36f195c729d4aff411bf0f
mcdevittp <mcdevittp1@mymail.nku.edu>
2024-01-21 12:55:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSC240_03_02_PhoneCall_pmm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
